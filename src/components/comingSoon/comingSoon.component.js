f83ff850b46897e9fef5d5b64c448a646b8568a9
arjunramesan <arjunramesan69@gmil.com>
2021-06-05 12:08:14 +0530

    comingSoon

---
+++ src/components/comingSoon/comingSoon.component.js
@@ -0,0 +1,25 @@
+  
+import React, { Component } from 'react';
+import "./comingSoon.component.css";
+import Wave from 'react-wavify'
+import logo from '../../assets/images/Logo.jpeg';
+
+export default class comingSoon extends Component {
+
+    render() {
+   
+      return (
+        <div className="comingSoonBody">
+          <div class="logoHolder">
+           <img className="logo" src={logo}></img>
+           <br></br>
+           <span className="logoText">Coming Soon</span>
+          </div>
+          <div className="waves">
+            <Wave className="wave1" fill='#f79902' options={{height: 100,amplitude: 100,speed: 0.20,points: 6}}/>
+            <Wave className="wave2" fill='#ededed' options={{height: 120,amplitude: 80,speed: 0.25,points: 5}}/>
+          </div>
+        </div>
+      );
+    }
+  }
